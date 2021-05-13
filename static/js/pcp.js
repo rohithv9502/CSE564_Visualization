dd461fcf3732a3045bee9978ea1f4719a3d40fb8
Rohith Vaddavalli <rohith@Rohiths-MacBook-Air.local>
2021-05-13 16:47:24 -0400

    Handle 0s

--- static/js/pcp.js
+++ static/js/pcp.js
@@ -16,9 +16,9 @@ function pcpplot(is_full_data,state){
 
 var devicePixelRatio = window.devicePixelRatio || 1;
 
-var pcp_color = d3.scaleOrdinal(d3.schemeCategory10)
+var pcp_color = d3.scaleOrdinal()
   .domain(["1", "2", "3", "4"])
-  //.range(["#B59248", "#50AB84", "#725D82", "#E15E5A"])
+  .range(["#87CEFA", "#00BFFF", "#1E90FF", "#4B0082"])
 //   .range(["#DB7F85", "#50AB84", "#4C6C86", "#C47DCB", "#B59248", "#DD6CA7", "#E15E5A", "#5DA5B3", "#725D82", "#54AF52", "#954D56", "#8C92E8", "#D8597D", "#AB9C27", "#D67D4B", "#D58323", "#BA89AD", "#357468", "#8F86C2", "#7D9E33", "#517C3F", "#9D5130", "#5E9ACF", "#776327", "#944F7E"]);
 
 var types = {
@@ -152,9 +152,9 @@ d3.json("/get-full-data", function(error, data) {
   if (error) throw error;
 
   data.forEach(function(d) {
-    dimensions.forEach(function(p) {
-      d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
-    });
+//    dimensions.forEach(function(p) {
+//      d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
+//    });
 
     // truncate long text strings to fit in data table
     for (var key in d) {
